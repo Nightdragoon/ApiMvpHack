7dd92d053b7d8355b5e91db25fc35b66e5d94241
Nightdragoon <crackytrini@gmail.com>
2026-02-26 01:13:53 -0600

    Crud completo caja
    se a puesto completo la caja

--- Dtos/UpdateCajaDto.py
+++ Dtos/UpdateCajaDto.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel , Field
 from datetime import date
-class CrearCajaDto(BaseModel):
+class UpdateCajaDto(BaseModel):
+    id: int = Field(... , title="id" , ge=1)
     idf_producto: int = Field(... , title="Producto" , ge=1)
     idf_empleado: int = Field(... , title="Empleado" , ge=1)
     dia: date = Field(default_factory=date.today, title="Dia")
--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from fastapi import FastAPI , Request
 from fastapi.exceptions import RequestValidationError
+from fastapi.params import Query
 from sqlalchemy import create_engine , select , update , delete , insert
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import sessionmaker
 from starlette.responses import JSONResponse
+from datetime import date
 
+from Dtos import UpdateCajaDto
 from Dtos.EmpleadoDto import EmpleadoDto
 from Dtos.UpdateEmpleadoDto import UpdateEmpleadoDto
-
+from Dtos.CrearCajaDto import CrearCajaDto
+from Dtos.UpdateCajaDto import UpdateCajaDto
+from typing import Optional
 engine = create_engine('sqlite:///./ProyectDb.db')
 
 Base = automap_base()
@@ -17,6 +22,7 @@ Base.prepare(engine , reflect=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Empleado = Base.classes.Empleados
+Caja = Base.classes.Caja
 app = FastAPI()
 
 @app.exception_handler(RequestValidationError)
@@ -121,6 +127,82 @@ async def get_all_empleados():
         if not empleados:
             return {"IsSuccess": False, "message": "no se encontraron empleados"}
         return {"IsSuccess": True, "message": "se encontraron los empleados", "data": empleados}
+    except Exception as e:
+        return {"IsSuccess": False, "message": str(e)}
+    finally:
+        db.close()
+
+#crud caja
+@app.get("/GetCaja")
+async def get_caja(fecha_inicial: Optional[date] = Query(default=None), fecha_final: Optional[date] = Query(default=None)):
+    db = SessionLocal()
+    try:
+        stmt = select(Base.classes.Caja)
+        if fecha_inicial is not None:
+            stmt = stmt.where(Base.classes.Caja.dia >= fecha_inicial)
+        if fecha_final is not None:
+            stmt = stmt.where(Base.classes.Caja.dia <= fecha_final)
+        result = db.execute(stmt)
+        caja = result.scalar_one_or_none()
+        if caja is None:
+            return {"IsSuccess": False, "message": "no se a encontrado la venta"}
+        result.close()
+        return {"IsSuccess": True, "message" : "se a encontrado la venta" , "data": caja}
+    except Exception as e:
+        return {"IsSuccess": False, "message" : str(e)}
+    finally:
+        db.close()
+
+@app.post("/PostCaja")
+async def post_caja(caja: CrearCajaDto):
+    db = SessionLocal()
+    try:
+        stmt = insert(Caja).values(idf_producto = caja.idf_producto , idf_empleado = caja.idf_empleado , dia = caja.dia).returning(Caja)
+        result = db.execute(stmt)
+        caja_creada = result.scalar_one_or_none()
+        if caja_creada is None:
+            result.close()
+            return {"IsSuccess": False, "message": "no se pudo crear la caja"}
+        result.close()
+        db.commit()
+        return {"IsSuccess": True, "message" : "se a creado la caja" , "data": caja_creada}
+    except Exception as e:
+        return {"IsSuccess": False, "message": str(e)}
+    finally:
+        db.close()
+
+@app.put("/UpdateCaja")
+async def update_caja(caja: UpdateCajaDto):
+    db = SessionLocal()
+    try:
+        stmt = update(Caja).where(Caja.id == caja.id).values(idf_producto = caja.idf_producto , idf_empleado = caja.idf_empleado , dia = caja.dia).returning(Caja)
+        result = db.execute(stmt)
+        caja_actualizada = result.scalar_one_or_none()
+        if caja_actualizada is None:
+            result.close()
+            return {"IsSuccess": False, "message": "no se encontro la caja"}
+        result.close()
+        db.commit()
+        return {"IsSuccess": True, "message": "se a actualizado la caja", "data": caja_actualizada}
+    except Exception as e:
+        return {"IsSuccess": False, "message": str(e)}
+    finally:
+        db.close()
+
+
+@app.delete("/DeleteCaja")
+async def delete_caja(id: int):
+    db = SessionLocal()
+    try:
+        stmt = delete(Caja).where(Caja.id == id).returning(Caja)
+        result = db.execute(stmt)
+        caja_eliminada = result.scalar_one_or_none()
+        if caja_eliminada is None:
+            result.close()
+            return {"IsSuccess": False, "message": "no se encontro la caja"}
+        result.close()
+        db.commit()
+        return {"IsSuccess": True, "message": "se a eliminado la caja", "data": caja_eliminada}
     except Exception as e:
         return {"IsSuccess": False, "message": str(e)}
     finally:
